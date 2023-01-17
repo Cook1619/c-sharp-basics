9d82b1d6c523905eb875a2ca78232b7887e012a5
Cook1619 <cook6348@gmail.com>
2023-01-16 19:35:36 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpBasics
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+            else
+                Console.WriteLine("The answer is less then 10");
+        }
+    }
+}
